030f9caa0abd09027879dcb9757cdfd650e5f3cd
nune2022 <nune.sargsyan15@gmail.com>
2022-03-19 17:26:07 +0400

    'sign up'

--- finish.py
+++ finish.py
@@ -1,12 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 driver = webdriver.Chrome("chromedriver")
-wait= WebDriverWait(driver,10)
+driver.implicitly_wait(10)
 driver.maximize_window()
 driver.get("https://www.phptravels.net/signup")
 driver.find_element_by_xpath("//*[@name='first_name']").send_keys("Nune")
